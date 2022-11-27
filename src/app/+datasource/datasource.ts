f2bc43722880b4e90a072ff38ef2ba5ee844462b
Mateusz Malz <mateusz.malz@gmail.com>
2022-11-27 19:19:52 +0100

    Commit

--- src/app/+datasource/datasource.ts
+++ src/app/+datasource/datasource.ts
@@ -2,15 +2,14 @@ import { BehaviorSubject, distinctUntilChanged, Observable, Subscription } from 
 
 export abstract class Datasource<T> {
 
-  private readonly data = new BehaviorSubject<T>(null);
+  private readonly data = new BehaviorSubject<T | null>(null);
   private readonly loading = new BehaviorSubject<boolean>(false);
   private readonly loaded = new BehaviorSubject<boolean>(false);
 
-  readonly data$: Observable<T> = this.data.pipe(distinctUntilChanged());
+  readonly data$: Observable<T | null> = this.data.pipe(distinctUntilChanged());
   readonly loading$: Observable<boolean> = this.loading.pipe(distinctUntilChanged());
   readonly loaded$: Observable<boolean> = this.loaded.pipe(distinctUntilChanged());
 
   private sub: Subscription = Subscription.EMPTY;
 
-  private load
 }
